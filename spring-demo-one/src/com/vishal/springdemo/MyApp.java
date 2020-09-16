0b87b3ae029996c8e9a91b1a639ad8d9ee7c7007
Vishal Kotak <vishalkt07@gmail.com>
2020-09-15 21:06:26 -0400

    Intial classes of the application with a BaseballCoach and MyApp as the main class

---
+++ spring-demo-one/src/com/vishal/springdemo/MyApp.java
@@ -0,0 +1,11 @@
+package com.vishal.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		BaseballCoach baseballCoach = new BaseballCoach();
+		
+		System.out.println(baseballCoach.getDailyWorkout());		
+	}
+}
